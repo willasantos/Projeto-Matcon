411546495a250c35a4cf7597fc35016e309be764
Willa Santos <wilasantos02@gmail.com>
2021-09-01 20:55:22 -0300

    Delete

--- models/clientes_model.py
+++ models/clientes_model.py
@@ -5,18 +5,32 @@ import sqlite3
 
 def getClientes():
     # crio a conexão
-    conn = sqlite3.connect('models/db_Banco de dados.db')
+    conn = sqlite3.connect('models/Bancodedados.db')
     # se comunica com o BD
     cursor = conn.cursor()
-    cursor.execute("SELECT * From clientes;")
-
+    # executa o comando de seleção dos clientes
+    cursor.execute("""SELECT * From clientes;""")
+    # coloca o resultado em uma lista de objetos clientes
+    lista_clientes = []
     for linha in cursor.fetchall():
         print(linha)
-
-    conn.close()    
-
+        id = linha[0]
+        nome = linha[1]
+        cpf = linha[2]
+        telefone = linha[3]
+        email = linha[4]
+        endereco = linha[5]
+        #cria novo objeto
+        novoCliente = Cliente(id, nome, cpf, telefone, email, endereco)
+        # insere o novo cliente na lista
+        lista_clientes.append(novoCliente)
+     # fecha a conexão
+    conn.close()  
+     # retorna a lista  
+    return lista_clientes
 
 def getCliente(id):
+    
     print("retorna um cliente específico")
 
 def addCliente(cliente):
